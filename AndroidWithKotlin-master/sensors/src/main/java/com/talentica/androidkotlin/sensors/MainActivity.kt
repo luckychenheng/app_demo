018ffae1c2950ec44dd22b6ea7275916fced350b
lucky <seemac@seedeMacBook-Pro.local>
2019-03-19 17:17:39 +0800

    no message

---
+++ AndroidWithKotlin-master/sensors/src/main/java/com/talentica/androidkotlin/sensors/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.talentica.androidkotlin.sensors
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    private var compass: Compass? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        try {
+            compass = Compass(this)
+        } catch (e:IllegalStateException) {
+            e.printStackTrace()
+            Toast.makeText(this, "Either accelerometer or magnetic sensor not found" , Toast.LENGTH_LONG).show()
+        }
+        compass?.arrowView = findViewById<ImageView>(R.id.main_image_hands)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        compass?.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        compass?.stop()
+    }
+
+    companion object {
+        private val TAG = "CompassActivity"
+    }
+
+}
+
